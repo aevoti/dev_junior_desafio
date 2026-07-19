ad83ffda2b01f56d9f3e41fb583ad76d75df3b1c
Gabriel Borges <gabriel.borges265@gmail.com>
2026-07-19 02:57:38 -0300

    feat: adiciona validacao na DTO de treinador

--- BackEnd/PokemonCenter/DTOs/TreinadorDtos.cs
+++ BackEnd/PokemonCenter/DTOs/TreinadorDtos.cs
@@ -3,8 +3,8 @@ using System.ComponentModel.DataAnnotations;
 namespace PokemonCenter.DTOs;
 
 public record CriarTreinadorRequest(
-        [property: Required] string Nome,
-        [property: Required, EmailAddress] string Email,
-        [property: Required] string CidadeDeOrigem);
+        [property: Required, StringLength(100)] string Nome,
+        [property: Required, EmailAddress, StringLength(150)] string Email,
+        [property: Required, StringLength(100)] string CidadeDeOrigem);
 
 public record TreinadorResponse(int Id, string Nome, string Email, string CidadeDeOrigem);
